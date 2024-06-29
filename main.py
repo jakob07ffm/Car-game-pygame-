eb5b5797d5fbc47bb25e69a3b83a6cad6686796f
Jakob__init__ <jsuetfels@gmail.com>
2024-06-29 12:44:57 +0200

    Update main.py
    Different approach

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
 import sys
+import math
 
 pygame.init()
 
-win = pygame.display.set_mode((800, 600))
+win = pygame.display.set_mode((1000, 1000))
 pygame.display.set_caption("Car_game")
 
 BLACK = (0, 0, 0)
@@ -13,14 +14,14 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
 
-car_width = 50
-car_height = 50
-car_x = 400 - car_width // 2
+car_x = 400 
 car_y = 400
-car_speed = 5
+car_speed = 0
 car_img_bad = pygame.image.load("car_pixel.png")
-car_img = pygame.transform.scale(car_img_bad, (200, 200))
+car_img = pygame.transform.scale(car_img_bad, (32, 52))
 car_angle = 0
+car_acceleration = 0.1
+car_turn_speed = 5
 
 clock = pygame.time.Clock()
 FPS = 60
@@ -34,33 +35,26 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_SPACE:
-                car_angle = 0
 
     keys = pygame.key.get_pressed()
 
     win.fill(GREEN)
 
     if keys[pygame.K_w]:
-        car_y -= car_speed
+        car_speed += car_acceleration
     if keys[pygame.K_s]:
-        car_y += car_speed
-    if keys[pygame.K_d]:
-        car_x += car_speed
-        if keys[pygame.K_SPACE]:
-            car_angle = 315
-    if keys[pygame.K_a]:
-        car_x -= car_speed
-
+        car_speed -= car_acceleration     
     
+    if keys[pygame.K_d]:
+        car_angle -= car_turn_speed
+       
+    if keys[pygame.K_a]:
+        car_angle += car_turn_speed
 
     rotated_car_img = pygame.transform.rotate(car_img, car_angle)
+    new_rect = rotated_car_img.get_rect(center=(car_x, car_y))
 
-    if car_angle > 0:
-        win.blit(rotated_car_img, (car_x, car_y))
-    else:
-        win.blit(car_img, (car_x, car_y))
+    win.blit(rotated_car_img, new_rect.topleft)    
     
     pygame.display.flip()
     
