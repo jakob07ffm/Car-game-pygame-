46cb6c10399ca839fd2b998542e54a3738ec9d8d
Jakob__init__ <jsuetfels@gmail.com>
2024-06-29 11:17:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ car_height = 50
 car_x = 400 - car_width // 2
 car_y = 400
 car_speed = 5
+car_img_bad = pygame.image.load("car_pixel.png")
+car_img = pygame.transform.scale(car_img_bad, (200, 200))
+car_angle = 0
 
 clock = pygame.time.Clock()
 FPS = 60
@@ -31,13 +34,33 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                car_angle = 0
 
     keys = pygame.key.get_pressed()
 
+    win.fill(GREEN)
+
     if keys[pygame.K_w]:
         car_y -= car_speed
+    if keys[pygame.K_s]:
+        car_y += car_speed
+    if keys[pygame.K_d]:
+        car_x += car_speed
+        if keys[pygame.K_SPACE]:
+            car_angle = 315
+    if keys[pygame.K_a]:
+        car_x -= car_speed
+
     
-    pygame.draw.rect(win, RED, (car_x, car_y, car_width, car_height))
+
+    rotated_car_img = pygame.transform.rotate(car_img, car_angle)
+
+    if car_angle > 0:
+        win.blit(rotated_car_img, (car_x, car_y))
+    else:
+        win.blit(car_img, (car_x, car_y))
     
     pygame.display.flip()
     
